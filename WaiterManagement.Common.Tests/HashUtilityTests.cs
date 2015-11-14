d00ac13415a902f6fc52c14ce7bfe87b7cb90914
ILoveCamelCasing <davidcvpl@gmail.com>
2015-11-14 17:07:08 +0100

    Włączenie Package Restore w projektach; Rozszerzenie trochę głównego okna w aplikacji menadżera; Przeniesienie klasy haszującej ze starego projektu, test na haszowanie;

---
+++ WaiterManagement.Common.Tests/HashUtilityTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WaiterManagement.Common.Security;
+
+namespace WaiterManagement.Common.Tests
+{
+    [TestClass]
+    public class HashUtilityTests
+    {
+        [TestMethod]
+        public void HashingTest()
+        {
+            var login = "login";
+            var password = "password";
+
+            var firstHash = HashUtility.CreateFirstHash(login, password);
+            var secondHash = HashUtility.CreateSecondHash(firstHash);
+
+            Assert.IsTrue(HashUtility.ValidatePassword(firstHash, secondHash));
+        }
+    }
+}
