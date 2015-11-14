af6adda4ea54c8fd167d192c74507e42f78bf757
ILoveCamelCasing <davidcvpl@gmail.com>
2015-11-14 19:11:58 +0100

    Encja użytkownika oraz aktywnego użytkownika, migracja

---
+++ WaiterManagement.Common/Entities/ActiveUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaiterManagement.Common.Entities.Abstract;
+
+namespace WaiterManagement.Common.Entities
+{
+    public class ActiveUser : IEntity
+    {
+        public int Id { get; private set; }
+        public Guid UserId { get; set; }
+        public Guid UserToken { get; set; }
+        public DateTime TokenCreation { get; set; }
+    }
+}
---
+++ WaiterManagement.DAL/Migrations/201511141805146_Users_UsersView_ActiveUsers.cs
@@ -0,0 +1,49 @@
+namespace WaiterManagement.DAL.Migrations
+{
+	using System;
+	using System.Data.Entity.Migrations;
+
+	public partial class Users_UsersView_ActiveUsers : DbMigration
+	{
+		public override void Up()
+		{
+			CreateTable(
+					"dbo.ActiveUsers",
+					c => new
+					{
+						Id = c.Int(nullable: false, identity: true),
+						UserId = c.Guid(nullable: false),
+						UserToken = c.Guid(nullable: false),
+						TokenCreation = c.DateTime(nullable: false),
+					})
+					.PrimaryKey(t => t.Id);
+
+			CreateTable(
+					"dbo.Users",
+					c => new
+					{
+						Id = c.Int(nullable: false, identity: true),
+						UserId = c.Guid(nullable: false),
+						SecondHash = c.String(),
+						CommonId = c.Guid(nullable: false),
+						Created = c.DateTime(nullable: false),
+						Modified = c.DateTime(),
+						Version = c.Int(nullable: false),
+						IsNewest = c.Boolean(nullable: false),
+						IsDeleted = c.Boolean(nullable: false),
+					})
+					.PrimaryKey(t => t.Id);
+
+			Sql(DbMigrationExtensions.CreateViewQuery("UsersView",
+				"SELECT [Id], [CommonId], [UserId], [SecondHash] From Users WHERE IsNewest=1 AND IsDeleted=0"));
+
+		}
+
+		public override void Down()
+		{
+			DropTable("dbo.UsersView");
+			DropTable("dbo.Users");
+			Sql(DbMigrationExtensions.DropViewQuery("UsersView"));
+		}
+	}
+}
