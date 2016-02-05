1aeb5f4a9dc216cd2b62a2a8700b5985c78fec7f
ILoveCamelCasing <davidcvpl@gmail.com>
2016-02-05 20:28:33 +0100

    Początki ReservationOrderSchedulera

---
+++ WaiterManagement.BLL/Events/Concrete/Service/ReservationOrderScheduled.cs
@@ -0,0 +1,10 @@
+﻿using WaiterManagement.BLL.Events.Base;
+using WaiterManagement.Common.Entities;
+
+namespace WaiterManagement.BLL.Events.Concrete.Service
+{
+	public class ReservationOrderScheduled : IEvent
+	{
+		public Order Order { get; set; }
+	}
+}
---
+++ WaiterManagement.BLL/Scheduling/Base/ReservationOrderScheduler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using WaiterManagement.BLL.Commands.Base;
+using WaiterManagement.BLL.Events.Concrete.Service;
+using WaiterManagement.Common.Entities;
+
+namespace WaiterManagement.BLL.Scheduling.Base
+{
+	public class ReservationOrderScheduler : Handler, IDisposable
+	{
+		#region Private Const Fields
+		private const int SchedulingInterval = 1000;
+		private const int ReservationInterval = 1000;
+		#endregion
+
+		#region Private Fields
+		private Timer _timer;
+		#endregion
+
+		#region Constructors
+
+		public ReservationOrderScheduler()
+		{
+			_timer = new Timer(OnTimerTick, null, 0, SchedulingInterval);
+		}
+		#endregion
+
+
+		#region Event Handlers
+		private void OnTimerTick(object state)
+		{
+			var awaitingOrders = UnitOfWork.GetWhere<ReservationOrder>(resO => resO.Status == ReservationOrderStatus.Created && (resO.ReservationTime - DateTime.Now).TotalMilliseconds < ReservationInterval);
+
+			if (awaitingOrders == null || !awaitingOrders.Any())
+				return;
+
+			foreach (var awaitingOrder in awaitingOrders)
+			{
+				UnitOfWork.Load(awaitingOrder, o => o.Order);
+				UnitOfWork.Load(awaitingOrder.Order, o => o.Table);
+
+				EventBus.PublishEvent(new ReservationOrderScheduled()
+				{
+					Order = awaitingOrder.Order
+				});
+
+				awaitingOrder.Status = ReservationOrderStatus.Finished;
+			}
+
+		}
+		#endregion
+
+
+		#region IDisposable
+		public void Dispose()
+		{
+			if (_timer != null)
+			{
+				_timer.Dispose();
+				_timer = null;
+			}
+		}
+		#endregion
+	}
+}
+
